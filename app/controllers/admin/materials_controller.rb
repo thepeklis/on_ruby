55e021f921da001bd841fc2c97f61b72532f59ef
phoet <phoetmail@googlemail.com>
2015-02-18 09:34:17 +0100

    add preview action

--- app/controllers/admin/materials_controller.rb
+++ app/controllers/admin/materials_controller.rb
@@ -1,2 +1,8 @@
 class Admin::MaterialsController < Admin::ResourcesController
+  def generate_preview
+    material = Material.find(params[:id])
+    material.generate_preview
+
+    redirect_to url_for(controller: "/admin/materials", action: :edit, id: material.id), notice: "Preview generated!"
+  end
 end
